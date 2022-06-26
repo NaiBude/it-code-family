ec529fff38e52904413adfe7f9a5cad0e62c3117
lucky <1561554125@qq.com>
2022-06-26 15:41:43 +0800

    注册页面

--- client/src/pages/apps/AppSingIn/singIn.tsx
+++ client/src/pages/apps/AppSingIn/singIn.tsx
@@ -1,173 +1,99 @@
-import React, { useState } from 'react';
-import { Input, Button } from 'tdesign-react';
+import React, { useRef, useEffect } from 'react';
+import { Form, Input, Button, MessagePlugin, Upload } from 'tdesign-react';
 import styles from './singIn.less';
-// import TipsId from './Tips/TipsId';
-import Tips from './Tips/TipsUser';
+import LogoPhoto from '../../../../assets/logo.png';
 
-export default function SingIn(props) {
-  // User
-  const [isUser, setIsUser] = useState(false);
-  // ID
-  const [IsShowId, setIsShowId] = useState(false);
-  // password
-  const [IsShow, setIsShow] = useState(false);
-  // user
-  const handleChangeUser = event => {
-    setIsUser(false);
-  };
-  const handleFocusUser = event => {
-    if (event === '') {
-      setIsUser(!isUser);
-    }
-  };
-  const handleBlurUser = event => {
-    if (event === '') {
-      setIsUser(!isUser);
-    }
-  };
-  // ID
-  const handleChangeID = event => {
-    console.log('isShowId', IsShowId);
+const { FormItem } = Form;
 
-    const reg = /^[0-9]{1,17}$/;
-    if (reg.test(event)) {
-      console.log(reg.test(event));
-      setIsShowId(false);
+const validateMessage = {
+  account: [
+    {
+      type: 'error',
+      message: '自定义用户名校验信息提示',
+    },
+  ],
+  description: [
+    {
+      type: 'warning',
+      message: '自定义个人简介校验信息提示',
+    },
+  ],
+};
+
+const rules: any = {
+  account: [{ required: true }, { min: 2 }, { max: 10, type: 'warning' }],
+  description: [
+    { validator: val => val.length < 10, message: '不能超过 20 个字，中文长度等于英文长度' },
+  ],
+  password: [{ required: true }, { len: 8, message: '请输入 8 位密码' }],
+};
+
+export default function BaseForm() {
+  const formRef = useRef();
+
+  const onSubmit = ({ validateResult, firstError }) => {
+    if (validateResult === true) {
+      MessagePlugin.success('提交成功');
     } else {
-      setIsShowId(true);
+      console.log('Errors: ', validateResult);
+      MessagePlugin.warning(firstError);
     }
-    if (event === '') {
-      setIsShowId(false);
-    } else {
-      setIsShowId(true);
-    }
-  };
-  const handleFocusID = event => {
-    if (event === '') {
-      setIsShowId(!IsShowId);
-    }
-  };
-  const handleBlurID = event => {
-    // if (event === '') {
-    //   setIsShowId(!IsShowId);
-    // }
-  };
-  //password
-  const handleChangePassword = event => {
-    console.log(event);
-  };
-  const handleChangeCpassword = event => {
-    console.log(event);
-  };
-  const handleClick = event => {
-    console.log(1);
   };
 
-  const handleFocus = event => {
-    setIsShow(!IsShow);
-  };
-
-  const handleBlur = () => {
-    setIsShow(!IsShow);
-  };
   return (
-    <div>
-      <div className={styles.register}>
-        <div>欢迎注册IMJ</div>
-        <div>
-          <span>昵称：</span>
-          <Input
-            align='left'
-            autoWidth={false}
-            autofocus={false}
-            clearable
-            defaultValue=''
-            disabled={false}
-            readonly={false}
-            showClearIconOnEmpty={false}
-            maxlength={10} //设置最大长度
-            size='medium'
-            tips={isUser && '昵称不能为空'}
-            type='text'
-            status={isUser ? 'error' : 'success'} //状态判定 成功success 失败error 信息不全error
-            onChange={handleChangeUser}
-            onFocus={handleFocusUser}
-            onBlur={handleBlurUser}
-          />
+    <div className={styles.form}>
+      <div className={styles.icon}>
+        <img className={styles.icon_img} src={LogoPhoto} alt='itmj' />
+      </div>
+      <div className={styles.text}>
+        <span className={styles.text_span}>用代码</span>
+        <p className={styles.text_p}>让世界变得更简单</p>
+      </div>
+      <div className={styles.d_form}>
+        <div className={styles.header_zc}>
+          <h3>欢迎注册</h3>
+          <p>
+            <span>
+              已有账号？<a>登录</a>
+            </span>
+          </p>
         </div>
-        <div>
-          <span>账号：</span>
-          <Input
-            align='left'
-            autoWidth={false}
-            autofocus={false}
-            clearable
-            defaultValue=''
-            disabled={false}
-            readonly={false}
-            showClearIconOnEmpty={false}
-            size='medium'
-            type='text'
-            tips={IsShowId ? '账号不能为空' : '账号必须为为8-16个数字'}
-            status={IsShowId ? 'error' : 'success'} //状态判定 成功success 失败error 信息不全error
-            onChange={handleChangeID}
-            onFocus={handleFocusID}
-            onBlur={handleBlurID}
-          />
-        </div>
-        <div>
-          <span>密码：</span>
-          <Input
-            align='left'
-            autoWidth={false}
-            autofocus={false}
-            clearable
-            defaultValue=''
-            disabled={false}
-            readonly={false}
-            showClearIconOnEmpty={false}
-            size='medium'
-            tips={IsShow && <Tips />}
-            type='password'
-            status='success'
-            onChange={handleChangePassword}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-        </div>
-        <div>
-          <span>确认密码：</span>
-          <Input
-            align='left'
-            autoWidth={false}
-            autofocus={false}
-            clearable
-            defaultValue=''
-            disabled={false}
-            readonly={false}
-            showClearIconOnEmpty={false}
-            size='medium'
-            type='password'
-            status='success'
-            onChange={handleChangeCpassword}
-          />
-        </div>
-        <div className={styles.button}>
-          <Button
-            block
-            disabled={false}
-            ghost={false}
-            loading={false}
-            shape='rectangle'
-            size='large'
-            type='submit'
-            variant='base'
-            theme='success'
-            onClick={handleClick}
+        <Form rules={rules} ref={formRef} onSubmit={onSubmit} scrollToFirstError='smooth'>
+          <FormItem label='用户名' help='这是用户名字段帮助说明' name='account'>
+            <Input placeholder='请设置用户名' />
+          </FormItem>
+          <FormItem label='密码' name='password'>
+            <Input type='password' placeholder='请设置密码' />
+          </FormItem>
+          <FormItem label='确认密码' name='password'>
+            <Input type='password' placeholder='请再次输入密码' />
+          </FormItem>
+          <FormItem label='个人简介' help='一句话介绍自己' name='description'>
+            <Input placeholder='简单介绍' />
+          </FormItem>
+          <FormItem label='头像' name='avatar'>
+            <Upload action='' theme='image' tips='请选择单张图片文件上传' accept='image/*'></Upload>
+          </FormItem>
+          <FormItem
+            style={{
+              marginLeft: 42,
+            }}
           >
-            立即注册
-          </Button>
-        </div>
+            <Button
+              theme='primary'
+              type='submit'
+              style={{
+                marginRight: 10,
+                borderRadius: 25,
+                width: 399,
+                height: 50,
+                backgroundColor: '#047e1c',
+              }}
+            >
+              提交
+            </Button>
+          </FormItem>
+        </Form>
       </div>
     </div>
   );
